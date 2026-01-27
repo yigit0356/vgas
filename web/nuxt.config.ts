3b092d2743a4c6828e902881ee3aa1186e2d862e
Yigit <yigitgurses2012@gmail.com>
2026-01-27 06:38:16 +0300

    Update nuxt.config.ts

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,14 +1,17 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
-  css: ['./app/assets/css/main.css'],
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    modules: ['@nuxt/eslint'],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    nitro: {
+        externals: {
+            inline: ['@prisma/client']
+        }
+    },
+    css: ['./app/assets/css/main.css']
 })
