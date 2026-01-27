98bae9a0c29f751145db46ddcbc74276e5482e55
Yigit <yigitgurses2012@gmail.com>
2026-01-27 06:41:18 +0300

    Update prisma.ts

--- web/server/utils/prisma.ts
+++ web/server/utils/prisma.ts
@@ -1,19 +1,19 @@
-import { PrismaClient } from '@prisma/client'
+import * as Prisma from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from 'pg'
 
 const prismaClientSingleton = () => {
-  const pool = new pg.Pool({ 
-    connectionString: process.env.DATABASE_URL 
-  })
-  const adapter = new PrismaPg(pool)
-  return new PrismaClient({ adapter })
+    const pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL
+    })
+    const adapter = new PrismaPg(pool)
+    return new Prisma.PrismaClient({ adapter })
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
+    prisma: PrismaClientSingleton | undefined
 }
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
